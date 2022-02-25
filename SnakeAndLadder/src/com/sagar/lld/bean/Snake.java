2fcb8ee84d52e6e4697a47cb7d861338b6e777d2
Sagar <Sagar@192.168.1.40>
2022-02-25 10:21:49 +0530

    First Commit

---
+++ SnakeAndLadder/src/com/sagar/lld/bean/Snake.java
@@ -0,0 +1,25 @@
+package com.sagar.lld.bean;
+
+public class Snake {
+	private int head;
+	private int tail;
+	
+	public Snake(int head,int tail) {
+		// TODO Auto-generated constructor stub
+		this.head=head;
+		this.tail=tail;
+	}
+	
+	public int getStart() {
+		return head;
+	}
+	
+	public int getEnd() {
+		return tail;
+	}
+	
+	
+	
+	
+
+}
